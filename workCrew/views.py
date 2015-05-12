43678b12acdb58ef273318325a25083a41262c00
AFortune <aaron.fortune@icloud.com>
2015-05-12 17:23:35 +0200

    I have added temporary buttons to the index page. The hope is that they can be linked to download/view functions in the heroku server

--- workCrew/views.py
+++ workCrew/views.py
@@ -1,12 +1,13 @@
 #from django.shortcuts import render
 import docCreator
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 def index(request):
     docCreator.docCreator()
    
-    output = 'success'
-    return HttpResponse(output)
+    
+    return render(request, 'workCrew/downloadSchedules.html')
 
 def allergies(request):
     output = "placeholder"
