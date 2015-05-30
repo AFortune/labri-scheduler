7c9361b671580f9fb76fa4294e0a2d75fcadbcd0
AFortune <aaron.fortune@icloud.com>
2015-05-30 11:46:19 +0200

    commit before working on the download feature

--- workCrew/datetest.py
+++ workCrew/datetest.py
@@ -1,3 +1,28 @@
-list = [1,2,3,4,5,6,7]
+def halfList(list, firstSecond):
+    firstHalf = []
+    secondHalf = []   
+    firstHalf = list[0:((len(list)-1)/2)]
+    secondHalf = list[((len(list) -1)/2): len(list)]
+    
+    if firstSecond == "firstHalf":
+        return firstHalf
+        
+    elif firstSecond == "secondHalf":
+        return secondHalf
+  
+array = ["aaron","david","Katrina", "Joseph", "Wyatt","Andrew", "Greg", "Wesley", "George", "ten", "eleven", "twelve", "George", "ten", "eleven", "twelve","12"]
+print halfList(array, "firstHalf")
+print ' '.join(halfList(array, "secondHalf"))
 
-print "hey " + str(len(list))
+def cycleAssignment(dict, jobList, namelist):
+    while x == 0:
+        print 1
+    
+testdict = {
+    "Grounds": ["aaron", "wyatt"],
+    "Cleaning": ["david", "joe"],
+
+}
+
+
+print testdict.keys()[0]
